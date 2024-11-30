67fe0669a0324b9d8cd802fcba565d5489127afe
rifatrahman1 <ahsan30rifat@gmail.com>
2024-11-30 22:41:46 +0600

    first commit

---
+++ JS/login.js
@@ -0,0 +1,7 @@
+const button_login = document.getElementById ('button_login');
+
+
+button_login.addEventListener ('click', function (event) {
+    event.preventDefault();
+    console.log('dilam click');
+})
